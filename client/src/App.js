b173b931a02aefdd6925c268761b786182f1fb67
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-22 14:05:55 -0600

    adding routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import SavedList from './Movies/SavedList';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
